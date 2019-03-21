c7f285f55da0f13babd26630f0997022d5fabadb
Tri Le <michaelle7312@gmail.com>
2019-03-21 03:05:25 -0400

    add some front end stuff

--- leftover-client/src/components/shop-list/shop-list.ts
+++ leftover-client/src/components/shop-list/shop-list.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController,ActionSheetController, ViewController, Events, AlertController} from 'ionic-angular';
-// import {RestProvider} from "../../providers/rest/rest";
 import {UserinfoProvider} from "../../providers/userinfo/userinfo";
 import { RestProvider } from '../../providers/rest/rest';
+import { ShoppingItem } from '../../models/item';
 @Component({
   selector: 'shop-list',
   templateUrl: 'shop-list.html'
@@ -12,6 +12,14 @@ export class ShopListComponent {
  
 
   question: string;
+
+  newItem: ShoppingItem = {
+	  name: "",
+	  amount: null,
+	  price: null,
+  }
+
+
   constructor(public navCtrl: NavController,
    public modalCtrl: ModalController, 
    public viewCtrl: ViewController,
@@ -23,7 +31,8 @@ export class ShopListComponent {
   	// this.getBookArr();
   }
 
-getBookArr(){
+
+getItemsArr(){
 	// this.neededBooks = this.usrInfo.getNeededBookArray();
 	// if(this.neededBooks.length <= 0){
 	// 	setTimeout(bad => {this.getBookArr()}, 1000)
@@ -40,65 +49,18 @@ closeModal(){
 }
 
 //ADD ENTRY OR MODIFY CURRENT ENTRY BASED ON THE BOOLEAN VARIABLE 
-addBook(){
-	// if(!this.bookISBN || !this.bookName){
-	// 	this.showAlert('you are missing either the ISBN or the name');
-	// }
-	// else if(!this.bookExchange && !this.bookBuy){
-	// 	this.showAlert('Please select at least one option for handling your book');
-	// }
-	// else if(this.bookBuy == true && (this.bookPrice == null || this.bookPrice == 0)){
-	// 	this.showAlert('Invalid price, please enter a valid price');
-	// }
-	// else if(String(this.bookISBN).length != 13){
-	// 	this.showAlert('Invalid ISBN number');
-	// }
-	// else{
-	// 	var method;
-	// 	if (this.bookBuy && this.bookExchange){
-	// 		method = 3
-	// 	}
-	// 	else if (this.bookBuy){
-	// 		method = 2
-	// 	}
-	// 	else{
-	// 		method = 1
-	// 	}
-	// 	var newBookObj = {
-	// 		isbn:this.bookISBN,
-	// 		name:this.bookName,
-	// 		buy:this.bookBuy,
-	// 		exchange:this.bookExchange,
-	// 		price: this.bookPrice
-	// 	}
-	// 	console.log("foobar", this.usrInfo.getUsrId());
-	// 	if(!this.bookBuy){
-	// 		this.bookPrice = 0;
-	// 	}
-	// 	var apiObj = {
-	// 		isbn: this.bookISBN,
-	// 		bName: this.bookName,
-	// 		method: method,
-	// 		price: this.bookPrice,
-	// 		uid: this.usrInfo.getUsrId()
-	// 	}
-	// 	if(this.currentEdit){
-	// 		apiObj["oldisbn"] = this.oldISBN;
-	// 		console.log("fooo", this.oldISBN);
-	// 		this.neededBooks[this.currentIndex] = newBookObj;
-	// 		// this.postReq(apiObj,'/editNeededBook')
-	// 		this.currentEdit = false;
-	// 	}
-	// 	else{
-	// 		this.neededBooks.push(newBookObj);
-	// 		// this.postReq(apiObj,'/postNeedBook')
-	// 	}
-	// 	this.bookISBN = null;
-	// 	this.bookName = "";
-	// 	this.bookExchange = true;
-	// 	this.bookBuy = false;
-	// 	this.bookPrice = null;
-	// }
+addItem(){
+	console.log('item', this.newItem)
+
+	//*TODO Kevin: Add this.newItem to the backend here
+	
+	
+	//END of backend work, time to reset this.newItem
+	this.newItem = {
+		name: "",
+		amount: null,
+		price: null,
+	}
 }
 
 // deleteBook(bookInfo){
@@ -148,31 +110,6 @@ confirmDeletion(bookInfo){
 	// 	]
 	// });
 	// alertCtrl.present();
-}
-
-// postReq(obj, url){
-// 	this.restAPI.postRequest(obj,url).then((result) => {
-// 		console.log(result);
-// 	},
-// 	(err) => {
-// 		console.log("error,err")
-// 	}
-// 	)
-// }
-
-editBook(bookInfo){
-// 	this.currentEdit = true;
-// 	this.bookBuy = bookInfo.buy;
-// 	this.bookExchange = bookInfo.exchange;
-// 	this.bookISBN = bookInfo.isbn;
-// 	this.bookPrice = bookInfo.price;
-// 	this.bookName = bookInfo.name;
-// 	for(var i in this.neededBooks){
-// 		if(this.neededBooks[i].isbn == bookInfo.isbn){
-// 			this.oldISBN = this.neededBooks[i].isbn;
-// 			this.currentIndex = parseInt(i);
-// 	}
-// 	}
 }
 
 /*****************API STUFF */
---
+++ leftover-client/src/models/item.ts
@@ -0,0 +1,5 @@
+export interface ShoppingItem {
+    name: string,
+    amount: number,
+    price?: number,
+}
